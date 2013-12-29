f838ef8d16c374e686a032f2c8f79d5e009b50d9
Amitay Dobo <amitayd@gmail.com>
2013-12-29 21:39:55 +0200

    fixed not supported message

--- js/app.js
+++ js/app.js
@@ -11,19 +11,24 @@ function synthogramInit() {
   console.log(model);
   var view = sgView(model, eventReporter);
   var source = new CanvasSource(view.paintCanvas, view.overlayCanvas, model.get('numOscillators'));
-  var synth = new OscSynth(
-    model.get('numOscillators'),
-    model.get('startNote'),
-    model.get('startOctave'),
-    model.get('musicalScale'),
-    model.get('numOctaves'),
-    model.get('volume'),
-    model.get('delayTime'),
-    model.get('delayFeedbackGain'),
-    model.get('delayWetGain'),
-    model.get('waveShape'),
-    model.get('isSynthPlaying')
-  );
+  var synth = null;
+  try {
+    synth = new OscSynth(
+      model.get('numOscillators'),
+      model.get('startNote'),
+      model.get('startOctave'),
+      model.get('musicalScale'),
+      model.get('numOctaves'),
+      model.get('volume'),
+      model.get('delayTime'),
+      model.get('delayFeedbackGain'),
+      model.get('delayWetGain'),
+      model.get('waveShape'),
+      model.get('isSynthPlaying')
+    );
+  } catch (ex) {
+    // synth stays null 
+  }
   var sequencer = new Sequencer(synth, source,
     model.get('stepsPerSecond'), model.get('currentStep')
   );
